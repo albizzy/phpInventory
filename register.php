e83a3285aa0bc307ad97e91160ff7010435c7402
Albert Mwasisoba <almwassy@gmail.com>
2022-05-18 06:34:56 +0300

    handled sqli query, page redirect and errors.

--- register.php
+++ register.php
@@ -8,8 +8,20 @@
         $email = $_POST['email'];
         $pass = password_hash($_POST['pass'], $_POST['pass2'],PASSWORD_DEFAULT);
 
-        //
+        // Check if the user already exists
         $sql = "INSERT INTO users (firstname, lastname, phone, email, pass) VALUES ('$firstname', '$lastname', '$phone', '$email', '$pass')";
+
+        // Check if the query is successful
         $connect = mysqli_query($link_to_db, $sql);
+
+        // If the query is successful, redirect to the login page
+        if($connect) {
+            header('Location: login.php');
+            die();
+        }
+
+        else {
+            echo "User register was unsuccessful!";
+        }
     }
 ?>
