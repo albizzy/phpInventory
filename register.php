761e2e3c1a228136df8cd1fc8970c4205f9c5660
Albert Mwasisoba <almwassy@gmail.com>
2022-05-18 06:29:41 +0300

    handles user input to the database

--- register.php
+++ register.php
@@ -7,5 +7,9 @@
         $phone = $_POST['phone'];
         $email = $_POST['email'];
         $pass = password_hash($_POST['pass'], $_POST['pass2'],PASSWORD_DEFAULT);
+
+        //
+        $sql = "INSERT INTO users (firstname, lastname, phone, email, pass) VALUES ('$firstname', '$lastname', '$phone', '$email', '$pass')";
+        $connect = mysqli_query($link_to_db, $sql);
     }
 ?>
