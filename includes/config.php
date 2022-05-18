1f72937d34b28cac07d3e06ec5405481888cf04e
Albert Mwasisoba <almwassy@gmail.com>
2022-05-18 06:16:47 +0300

    inital commit with includes and register page

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?
+    define("SERVER","localhost");
+    define("USER","root");
+    define("PASS", "");
+    define("DB", "inventory");
+
+    $link_to_db = mysqli_connect(SERVER, USER, PASS, DB);
+?>
