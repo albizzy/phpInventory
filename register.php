ae494a4be826fd7ede1077d0b20ad0896afebb91
Albert Mwasisoba <almwassy@gmail.com>
2022-05-18 06:37:32 +0300

    added a form for user inputs and submission

--- register.php
+++ register.php
@@ -25,3 +25,28 @@
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get started.</title>
+</head>
+
+<body>
+    <form action="register.php" method="POST" autocomplete="off">
+        <h1>Create account</h1>
+        <input type="text" name="firstname" id="fname" placeholder="First Name"><br>
+        <input type="text" name="lastname" id="lname" placeholder="Last Name"><br>
+        <input type="text" name="phone" id="phone" placeholder="Phone Number"><br>
+        <input type="email" name="email" id="email" placeholder="Email"><br>
+        <input type="password" name="pass" id="pass" placeholder="Password"><br>
+        <input type="submit" name="send" value="Sign Up">
+        <input type="text" name="hidden" autocomplete="false" style="display: none;"><br><br>
+    </form>
+</body>
+
+</html>
